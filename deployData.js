6068a84b034d9bacfa00ba54ae7787fcdd7f5c42
pikalupps <putudhikap@gmail.com>
2023-05-18 18:38:30 +0700

    init fp

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Putu Andhika Pratama'; // TODO: replace this
+
+const studentId = 'FE5597365'; // TODO: replace this
+
+const NetlifyDeployUrl = 'https://halostudent.netlify.app/'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
